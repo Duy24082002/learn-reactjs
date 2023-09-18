cfde67f475b88fd4f11e8e0c05018342b8a3d343
Duy <20521248@gm.uit.edu.vn>
2023-09-18 16:29:14 +0700

    Update name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Duc Duy
+          Hello Mr.Binh
         </p>
         <a
           className="App-link"
